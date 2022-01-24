e310b1cf3b4d8bdbd3481e043dfb96bda6094a08
Prajwal P <prajwalprakash3722@gmail.com>
2022-01-25 00:40:55 +0530

    [modify] Modified the Content

--- pages/index.tsx
+++ pages/index.tsx
@@ -4,22 +4,22 @@ import Typewriter from "typewriter-effect";
 export default function Home() {
   return (
     <div className="relative container w-[80%] md:w-[60%] mx-auto py-[5rem] min-h-screen">
-      <div className="logo mb-12 h-16 w-16 p-1 sm:h-20 sm:w-20 rounded-full bg-gradient-to-r from flex items-center justify-center">
+      <div className="logo mb-4 md:mb-12 h-24 w-24 p-1 sm:h-20 sm:w-20 rounded-full bg-gradient-to-r from flex items-center justify-center">
         <Image
           src="/prajwal.PNG"
           alt="Prajwal P"
-          className="sm:h-[4.5rem] sm:w-[4.5rem] rounded-full border-2 object-cover"
+          className="sm:h-[4.5rem] sm:w-[4.5rem] rounded-full object-cover"
           width={400}
           height={400}
         />
       </div>
       <div className="flex flex-row">
-        <h1 className="text-xl sm:text-3xl text-white my-5 font-display">
+        <h1 className="text-xl sm:text-3xl text-white my-4 font-display">
           Hola, I am{" "}
           <span className="text-[#f19050] font-bold text-4xl">
             <Typewriter
               options={{
-                strings: ["Prajwal Prakash", , "Prajwal P"],
+                strings: ["Prajwal Prakash", "Prajwal P"],
                 autoStart: true,
                 loop: true,
                 deleteSpeed: 15,
@@ -28,29 +28,41 @@ export default function Home() {
           </span>
         </h1>
       </div>
-      <p className="text-white sm:full lg:w-3/4 text-base sm:text-xl font-body">
-        I am a{" "}
-        <span className="text-[#50AEF1] font-bold">Computer Programmer</span>,
-        <span className="text-[#50AEF1] font-bold"> Full Stack Developer</span>{" "}
-        &&nbsp;
-        <span className="text-[#50AEF1] font-bold">Problem Solver</span>. I like
-        to develop pretty sustainable solutions using latest technologies.&nbsp;
-        Till now Most of my work has been in Web Development and Backend
-        Engineering.&nbsp; I am very keenly interested in&nbsp;
-        <span className="text-[#50AEF1] font-bold">Operating Systems</span>
-        &nbsp; as of now
-      </p>
-      <p className="text-white sm:full lg:w-3/4 text-base sm:text-xl my-4 font-body">
-        I am currently a second year undergrad student at&nbsp;
+      <p className="text-white sm:full lg:w-3/4 text-base sm:text-xl font-body first-letter:text-2xl leading-relaxed lg:leading-1.8">
+        {`I'm a`} 2nd year CSE undergrad student from&nbsp;
         <span className="text-[#50AEF1] font-bold">
           R V College Of Engineering
         </span>
-        , studying&nbsp;
-        <span className="text-[#50AEF1] font-bold">
-          Bachelors in Computer Science Engineering
-        </span>
-        .
+        .&nbsp;I like to develop pretty sustainable solutions using latest
+        technologies.&nbsp;Till now Most of my work has been in&nbsp;
+        <span className="text-[#50AEF1] font-bold">Web Development</span>
+        and&nbsp;
+        <span className="text-[#50AEF1] font-bold">Backend Engineering</span>
+        .&nbsp; I am very keenly interested in&nbsp;
+        <span className="text-[#50AEF1] font-bold">Operating Systems</span>
+        &nbsp;&&nbsp;
+        <span className="text-[#50AEF1] font-bold">Data Structures</span>
+        &nbsp; as of now.
       </p>
+
+      <div className="flex flex-row justify-center items-center mt-4">
+        <span className="text-[#f19050] font-bold text-ellipsis text-xl">
+          <Typewriter
+            options={{
+              strings: [
+                "Computer Programmer",
+                "Full Stack Developer",
+                "Problem Solver",
+              ],
+              autoStart: true,
+              loop: true,
+              deleteSpeed: 15,
+            }}
+          />
+        </span>
+        &nbsp; &nbsp;
+      </div>
+
       <div className="text-[#ecdcdc] my-8 flex bg-black rounded-lg bg-opacity-30 p-4 backdrop-filter backdrop-blur-md lg:w-1/4 justify-evenly">
         <a href="https://www.github.com/prajwalprakash3722" target="blank">
           <Image
