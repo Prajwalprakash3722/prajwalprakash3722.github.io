1bb5d44603e94e7af28d2de45e3c4072bec1990b
Prajwalprakash3722 <prajwalprakash3722@gmail.com>
2024-10-06 08:01:22 +0000

    Deploying to gh-pages from @ Prajwalprakash3722/prajwalprakash3722.github.io@8444530a941ccb790803122fecaa2115598a2d84 🚀

---
+++ _next/static/quLYsiathNs-1ZuFpkasR/_buildManifest.js
@@ -0,0 +1,1 @@
+self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/pages/index-a54e7ee09bce2ef2.js"],"/404":["static/chunks/pages/404-bf4e1c9ec97f43dd.js"],"/_error":["static/chunks/pages/_error-2280fa386d040b66.js"],sortedPages:["/","/404","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
